cd0c0eed1f0ffd8f534440f470c7009a76b5253d
Kirill Antropov <chs4t-740@mail.ru>
2022-11-22 17:59:13 +0300

    Added Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,17 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+int num1 = 10;
+int num2 = 100;
+
+if (num1 * num1 == num2)
+{
+    Console.Write($"{num2} is square of number {num1}");
+}
+else if (num2 * num2 == num1)
+{
+    Console.Write($"{num1} is square of number {num2}");
+}
+else
+{
+    Console.Write("No");
+}

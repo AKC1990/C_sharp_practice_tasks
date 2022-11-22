5b13baee0f5b1b26838588dfd978b53546f9bc9f
Kirill Antropov <chs4t-740@mail.ru>
2022-11-22 19:04:05 +0300

    Added Task19

---
+++ Task19/Program.cs
@@ -0,0 +1,28 @@
+﻿// Определить номер четверти плоскости, в которой находится точка с координатами Х и У, причем X ≠ 0 и Y ≠ 0
+
+int x = -10;
+int y = 18;
+
+if (x != 0 && y != 0)
+{
+    if (x > 0 && y > 0)
+    {
+        Console.WriteLine("The second quarter");
+    }
+    else if (x < 0 && y > 0)
+    {
+        Console.WriteLine("The first quarter");
+    }
+    else if (x > 0 && y < 0)
+    {
+        Console.WriteLine("The third quarter");
+    }
+    else
+    {
+        Console.WriteLine("The fourth quarter");
+    }
+}
+else 
+{
+    Console.WriteLine("x or y can`t be 0");
+}

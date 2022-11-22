0303035756597fc069b52cf3617afee475ff72a0
Kirill Antropov <chs4t-740@mail.ru>
2022-11-22 16:40:13 +0300

    Added Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня недели выходным 
+
+int day_number = 5;
+if (1 <= day_number && day_number <=5)
+{
+    Console.WriteLine("Weekdays");
+}
+if (6 <= day_number && day_number <=7)
+{
+    Console.WriteLine("Weekend!!!");
+}

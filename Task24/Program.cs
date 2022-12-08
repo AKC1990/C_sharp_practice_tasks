e5d9f14467e60ea88e9f3866a11f3b96b960942f
Kirill Antropov <chs4t-740@mail.ru>
2022-12-08 12:25:11 +0300

    Added Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,30 @@
+﻿// Найти кубы чисел от 1 до N
+int[] CreateArray(int N)
+{
+    return new int[N];
+}
+
+int[] FillArray(int[] array)
+{
+    for (int i = 1; i <= array.Length; i++)
+    {
+        array[i-1] = i * i * i;
+    }
+    return array;
+}
+
+string PrintArray(int[] array)
+{
+    string result = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        result += $"{array[i]} ";
+    }
+    return result + "]";
+}
+
+Console.WriteLine("Enter a number");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] my_array = CreateArray(N);
+FillArray(my_array);
+Console.WriteLine(PrintArray(my_array));
